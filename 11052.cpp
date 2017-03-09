1aa1e4d05e5c8e5f5822da64a9d21e86cead5f85
ddalko <xocmdh1562@gmail.com>
2017-03-10 00:25:32 +0900

    11052

--- 11052.cpp
+++ 11052.cpp
@@ -1,1 +1,22 @@
 #include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int p[1002];
+int dp[1002];
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;++i) scanf("%d",p+i);
+
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=i;++j){
+            dp[i] = max(dp[i], dp[i-j]+p[j]);
+        }
+    }
+
+    printf("%d\n",dp[n]);
+}
